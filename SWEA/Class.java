fce8319fdac182532a24a1a78fca572a2f81a26b
jayl2sw <jayl2sf@gmail.com>
2022-01-18 00:11:20 +0900

    2022-01-17

---
+++ SWEA/Class.java
@@ -0,0 +1,12 @@
+// 주석 달기
+/* 
+여러줄 주석 달기
+*/
+public class Class{
+    // 변수 선언
+    // 메서드 선언
+    public static void main(String args[]){
+        // 실행될 프로그램 코드
+        System.out.println("첫번째로 작성한 자바 프로그램");
+    }
+}
