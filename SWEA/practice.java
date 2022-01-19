8d82a89fc2be2e842abf3b031f03783e37d4af24
jayl2sw <jayl2sf@gmail.com>
2022-01-19 23:03:54 +0900

    2022-01-19 array

--- SWEA/practice.java
+++ SWEA/practice.java
@@ -2,11 +2,16 @@
 /* 
 여러줄 주석 달기
 */
-public class Class{
+public class practice{
     // 변수 선언
     // 메서드 선언
-    public static void main(String args[]){
+    public static void main(String ard[]){
         // 실행될 프로그램 코드
         System.out.println("첫번째로 작성한 자바 프로그램");
+        int count = 1;
+        int total = ++count;
+        
+        System.out.println(count+total);
+
     }
 }
